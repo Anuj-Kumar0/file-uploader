9209eaf2ece5e826aafb368c0ce8c1cbf5995767
Anuj Kumar <anujkumar21052004@gmail.com>
2026-03-20 08:46:39 +0530

    feat: modified application logic to add file size and display folders

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,6 +1,34 @@
-export const uploadFile = (req, res) => {
-    res.json({
-      message: "File uploaded",
-      file: req.file,
+import { prisma } from "../../lib/prisma.js";
+
+export const getUploadPage = async (req, res) => {
+  try {
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: req.user.id,
+      },
     });
-  };
+
+    res.render("upload", { folders });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error loading upload page");
+  }
+};
+
+export const uploadFile = async (req, res) => {
+  const { folderId } = req.body;
+
+  if (!req.file) return res.send("No file");
+
+  await prisma.file.create({
+    data: {
+      filename: req.file.originalname,
+      path: req.file.path,
+      size: req.file.size,
+      userId: req.user.id,
+      folderId: folderId ? Number(folderId) : null,
+    },
+  });
+
+  res.redirect("/folders");
+};
