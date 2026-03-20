48e6bc80d0fd088e1c18bcda696702ad2828019a
Anuj Kumar <anujkumar21052004@gmail.com>
2026-03-20 08:47:33 +0530

    feat: modified upload routes to show folders

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { uploadFile } from "../controllers/uploadController.js";
+import { uploadFile, getUploadPage } from "../controllers/uploadController.js";
 import { isAuthenticated } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", isAuthenticated, (req, res) => {
-    res.render("upload");
-  });
-  
-  router.post("/", isAuthenticated, upload.single("file"), uploadFile);
+router.get("/", isAuthenticated, getUploadPage);
+
+router.post("/", isAuthenticated, upload.single("file"), uploadFile);
 
 export default router;
