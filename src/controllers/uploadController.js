627d33de62b494634d8775d0359552c619240244
Anuj Kumar <anujkumar21052004@gmail.com>
2026-03-21 08:52:23 +0530

    feat: upload on supabase

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,4 +1,6 @@
 import { prisma } from "../../lib/prisma.js";
+import supabase from "../../lib/supabase.js";
+import fs from "fs";
 
 export const getUploadPage = async (req, res) => {
   try {
@@ -20,15 +22,39 @@ export const uploadFile = async (req, res) => {
 
   if (!req.file) return res.send("No file");
 
+  const fileBuffer = fs.readFileSync(req.file.path);
+
+  const fileName = Date.now() + "-" + req.file.originalname;
+
+  // upload to supabase
+  const { data, error } = await supabase.storage
+    .from("files")
+    .upload(fileName, fileBuffer);
+
+    if (error) {
+      console.error("Supabase upload error:", error);
+      return res.send("Upload failed: " + error.message);
+    }
+
+  // get public URL
+  const { data: publicUrl } = supabase.storage
+    .from("files")
+    .getPublicUrl(fileName);
+
+  // save in DB
   await prisma.file.create({
     data: {
       filename: req.file.originalname,
-      path: req.file.path,
+      url: publicUrl.publicUrl,
+      path: fileName, 
       size: req.file.size,
       userId: req.user.id,
       folderId: folderId ? Number(folderId) : null,
     },
   });
+
+  // delete local file
+  fs.unlinkSync(req.file.path);
 
   res.redirect("/folders");
 };
