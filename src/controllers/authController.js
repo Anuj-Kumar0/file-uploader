77ba42077cb734814e34167a07dc30ca3b7a56af
Anuj Kumar <anujkumar21052004@gmail.com>
2026-03-30 16:51:11 +0530

    feat: added business logic for login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,34 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../../lib/prisma.js";
+import passport from "passport";
+import { validationResult } from "express-validator";
 
 export const registerUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render("register", {
+      errors: errors.array(),
+      old: req.body,
+    });
+  }
+
   const { email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.render("register", {
+        errors: [{ msg: "Email already exists" }],
+        old: req.body,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await prisma.user.create({
       data: {
         email,
@@ -17,8 +39,25 @@ export const registerUser = async (req, res) => {
     res.redirect("/login");
   } catch (err) {
     console.error(err);
-  res.status(500).send(err.message);
+    res.status(500).send(err.message);
   }
+};
+
+export const loginUser = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) {
+      return res.render("login", {
+        error: info.message,
+      });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/upload");
+    });
+  })(req, res, next);
 };
 
 export const logoutUser = (req, res, next) => {
